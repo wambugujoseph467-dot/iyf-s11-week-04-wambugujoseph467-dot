0ff324c20618f793b50eb82bd42b47ae43b66c9e
Joseph Wambugu <wambugujoseph467@gmail.com>
2026-07-06 13:06:47 +0300

    feat: complete Task 7.1 JavaScript setup and variables

---
+++ main.js
@@ -0,0 +1,9 @@
+// Variable declarations
+let name = "Joseph Wambugu";
+let age = 20;      // 
+const birthYear = 2006;   // 
+
+// typeof operator
+console.log(typeof name);      // string
+console.log(typeof age);       // number
+console.log(typeof true);      // boolean
